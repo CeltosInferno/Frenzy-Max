0b1fe0b438e83adbe2c3b8db04db2b25069eaf2c
ttgc <ttgc@users.noreply.github.com>
2020-02-28 18:31:58 -0500

    Added sound manager

---
+++ Assets/Scripts/PersistentGameObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentGameObject : MonoBehaviour
+{
+    public string objectTag;
+
+    void Awake()
+    {
+        if (GameObject.FindGameObjectsWithTag(objectTag).Length > 1)
+        {
+            Kill();
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void Kill()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer = null;
+    [SerializeField] private AudioMixerGroup musicMixerGroup = null;
+    [SerializeField] private AudioMixerGroup soundMixerGroup = null;
+
+    private AudioSource music;
+    private readonly List<AudioSource> sound = new List<AudioSource>();
+    private readonly List<AudioClip> queue = new List<AudioClip>(10);
+
+    public static SoundManager instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        audioMixer.SetFloat("masterVolume", PlayerPrefs.GetFloat("masterVolume", 0));
+        audioMixer.SetFloat("musicVolume", PlayerPrefs.GetFloat("musicVolume", -30));
+        audioMixer.SetFloat("soundVolume", PlayerPrefs.GetFloat("soundVolume", 0));
+
+        AudioSource[] src = GetComponents<AudioSource>();
+        Debug.Assert(src.Length >= 2);
+        if (src.Length < 2) Application.Quit();
+
+        music = src[0];
+        music.loop = true;
+        music.outputAudioMixerGroup = musicMixerGroup;
+
+        sound.Capacity = src.Length;
+        sound.AddRange(src);
+        sound.Remove(src[0]);
+        sound.Capacity = src.Length - 1;
+        sound.ForEach(s => s.loop = false);
+        sound.ForEach(s => s.outputAudioMixerGroup = soundMixerGroup);
+    }
+
+    private void Update()
+    {
+        if (queue.Count > 0)
+        {
+            AudioSource src = sound.Find(s => !s.isPlaying);
+            if (src != null)
+            {
+                src.clip = queue[0];
+                queue.RemoveAt(0);
+                src.Play();
+            }
+        }
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        if (music.clip != clip)
+        {
+            music.clip = clip;
+            music.Play();
+        }
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        AudioSource src = sound.Find(s => !s.isPlaying);
+        if (queue.Count > 0 || src == null)
+        {
+            queue.Add(clip);
+        }
+        else
+        {
+            src.clip = clip;
+            src.Play();
+        }
+    }
+
+}
