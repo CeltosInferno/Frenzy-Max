0a78d808ceb214b42b0921f11e0eff304484f610
ttgc <ttgc@users.noreply.github.com>
2020-02-29 18:18:49 -0500

    Converted pool into spawner

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,48 +4,50 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    internal class PoolItemStruct
-    {
-        public GameObject Object { get; set; }
-        public bool Locked { get; set; }
-    }
-
     [SerializeField] private int poolSize = 10;
     [SerializeField] private GameObject prefab = null;
-    private List<PoolItemStruct> pool;
+    private List<GameObject> pool;
     public float spawnTime = 10.0f;
     public float randomSpawnTimeVariation = 1.0f;
+    private float realSpawnTime;
+    private float internalTime = 0.0f;
 
     public int PoolSize { get { return poolSize; } }
 
     // Start is called before the first frame update
     void Start()
     {
-        pool = new List<PoolItemStruct>(poolSize);
+        pool = new List<GameObject>(poolSize);
         for (int i=0 ; i < poolSize ; i++)
         {
             var obj = Instantiate(prefab);
             obj.SetActive(false);
-            pool.Add(new PoolItemStruct() { Object = obj, Locked = false });
+            pool.Add(obj);
+        }
+
+        realSpawnTime = spawnTime;
+    }
+
+    void Update()
+    {
+        internalTime += Time.deltaTime;
+
+        if (internalTime >= realSpawnTime)
+        {
+            Spawn();
+            internalTime = 0.0f;
+            realSpawnTime = spawnTime + Random.Range(0.0f, randomSpawnTimeVariation);
         }
     }
 
-    public int Available { get { return pool.FindAll(it => !it.Locked).Count; } }
+    public int Available { get { return pool.FindAll(it => !it.activeInHierarchy).Count; } }
 
-    public GameObject Spawn()
+    private void Spawn()
     {
-        if (Available <= 0) return null;
+        if (Available <= 0) return;
 
-        var obj = pool.Find(it => !it.Locked);
-        obj.Locked = true;
-        return obj.Object;
-    }
-
-    public void Release(GameObject obj)
-    {
-        var objStruct = pool.Find(it => it.Object == obj);
-        Debug.Assert(objStruct != null);
-        objStruct.Locked = false;
-        obj.SetActive(false);
+        var obj = pool.Find(it => !it.activeInHierarchy);
+        obj.transform.position = gameObject.transform.position;
+        obj.SetActive(true);
     }
 }
