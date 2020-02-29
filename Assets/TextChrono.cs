a4b0085517bd13edd685addc7607d76d71b99d3c
Rakazthas <guillaume.lsn@gmail.com>
2020-02-29 15:13:13 -0500

    Fix timer

--- Assets/TextChrono.cs
+++ Assets/TextChrono.cs
@@ -11,7 +11,7 @@ public class TextChrono : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        timer = FindObjectOfType<Timer>();
+        timer = GameObject.FindGameObjectWithTag("Manager").GetComponentInChildren<Timer>();
         textTimer = GetComponent<Text>();
         timer.StartTimer();
     }
