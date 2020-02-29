d5cf3fd2c38cfaa2c83e0c40f51dd58d1ad9da65
Grimpeaks <44480134+Grimpeaks@users.noreply.github.com>
2020-02-28 20:20:52 -0500

    Added State-Machine

---
+++ Assets/Scripts/AI/PluggableStateMachine/StateController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class StateController : MonoBehaviour
+{
+    public State currentState;
+
+    public int lookSphereCastRadius;
+    public int lookRange;
+
+    public Transform eyes;
+    public List<Transform> wayPointList;
+    public State remainState;
+
+    [HideInInspector] public NavMeshAgent navMeshAgent;
+    [HideInInspector] public int nextWayPoint;
+    [HideInInspector] public Transform chaseTarget;
+    [HideInInspector] public float stateTimeElapsed;
+
+    void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    public void SetupAI()
+    {
+
+    }
+
+    void Update()
+    {
+        currentState.UpdateState(this);
+    }
+
+    public void TransitionToState(State nextState)
+    {
+        if (nextState != remainState)
+        {
+            currentState = nextState;
+        }
+    }
+
+    public bool CheckIfCountDownElapsed(float duration)
+    {
+        stateTimeElapsed += Time.deltaTime;
+        return (stateTimeElapsed >= duration);
+    }
+
+    private void OnExitState()
+    {
+
+    }
+}
