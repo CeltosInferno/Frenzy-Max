d1cb288f63780a3868f543f331cb845823aa7ab3
Léo Le Corre <leo.le.corre@gmail.com>
2020-02-29 22:42:29 -0500

    Adding Highscore

---
+++ Assets/Scripts/DrawScore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawScore : MonoBehaviour
+{
+    public Text[] fields;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<int> scores = GameObject.FindGameObjectWithTag("Manager").GetComponentInChildren<HighScores>().HighScoreList;
+        for (int i=0 ; i < System.Math.Min(scores.Count, fields.Length) ; i++)
+        {
+            fields[i].text = Timer.TimeToString(scores[i]);
+        }
+    }
+}
