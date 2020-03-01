62b2abc10945cf26c6dd33197a76c0950e1b980a
Rakazthas <guillaume.lsn@gmail.com>
2020-02-29 22:33:40 -0500

    Victory death

---
+++ Assets/Scripts/AnimatorScripts/VictoryDeath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class VictoryDeath : StateMachineBehaviour
+{
+    private Text victoryMessage;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        victoryMessage = GameObject.FindObjectOfType<Text>();
+
+        victoryMessage.text += "\nOr not...";
+
+        SceneManager.LoadScene(0);
+    }
+}
